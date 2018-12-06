3fb09c6659a88619e25e83a08bc60fff83be59a7
Pavel Jiranek <pavel.jiranek@siemens.com>
2018-12-06 22:10:31 +0100

    Added day 1.

---
+++ aoc/2018/day1.py
@@ -0,0 +1,26 @@
+from aoc import utils
+
+
+def final_frequency(freq_changes):
+    freq = 0
+    for df in freq_changes:
+        freq += df
+    return freq
+
+
+def repeated_frequency(freq_changes):
+    freq = 0
+    freq_seen = set((freq, ))
+    while True:
+        for df in freq_changes:
+            freq += df
+            if freq in freq_seen:
+                return freq
+            else:
+                freq_seen.add(freq)
+
+
+freq_changes = [int(line) for line in utils.read_input('input1.txt')]
+
+print('Part 1:', final_frequency(freq_changes))
+print('Part 2:', repeated_frequency(freq_changes))
