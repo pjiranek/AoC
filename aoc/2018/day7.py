5fc05f1da15d8e334c0a58687485d6eefbbed1d2
Pavel Jiranek <pavel.jiranek@siemens.com>
2018-12-07 20:13:14 +0100

    Added day 7.

--- aoc/2018/day7.py
+++ aoc/2018/day7.py
@@ -1,48 +1,149 @@
 from aoc import utils
 from collections import defaultdict
+from abc import ABCMeta, abstractmethod
 import re
 
 
-def make_graph(lines):
-    pattern = r'Step ([A-Z]) must be finished before step ([A-Z]) can begin.'
-    expr = re.compile(pattern)
-    graph = defaultdict(lambda: {'succ': set(), 'prec': set()})
-    for line in lines:
-        match = expr.match(line)
-        assert match
-        node_from, node_to = match.groups()
-        graph[node_from]['succ'].add(node_to)
-        graph[node_to]['prec'].add(node_from)
-    return graph
+class StepSource:
+
+    def __init__(self, file_name):
+        pattern = r'Step ([A-Z]) must be finished before step ([A-Z]) can begin.'
+        expr = re.compile(pattern)
+
+        graph = defaultdict(lambda: {'succ': set(), 'prec': set()})
+        for line in utils.read_input(file_name):
+            match = expr.match(line)
+            node_source, node_target = match.groups()
+            graph[node_source]['succ'].add(node_target)
+            graph[node_target]['prec'].add(node_source)
+
+        self._graph = graph
+
+        self._order = []
+        self._processed = set()
+        self._todo = set(node
+                         for node in graph
+                         if len(graph[node]['prec']) == 0)
+
+    def get_next(self):
+        if len(self._todo) > 0:
+            step = min(self._todo)
+            self._todo.remove(step)
+            return step
+        else:
+            return None
+
+    def finish(self, step):
+        self._processed.add(step)
+        self._todo.update(node
+                          for node in self._graph[step]['succ']
+                          if self._graph[node]['prec'] <= self._processed
+                          if node not in self._processed)
+
+        self._order.append(step)
+
+    @property
+    def done(self):
+        return len(self._processed) == len(self._graph)
+
+    @property
+    def order(self):
+        return ''.join(self._order)
 
 
-def order_nodes(graph):
-    order = []
-    processed = set()
-    to_process = set(node for node in graph if len(graph[node]['prec']) == 0)
-    while len(to_process) > 0:
-        current = min(to_process)
-        processed.add(current)
-        to_process.remove(current)
+class StepDoer(metaclass = ABCMeta):
 
-        # Can add only successors of the current node which
-        # have already processed predecessors.
-        can_process_next = set(node
-                               for node in graph[current]['succ']
-                               if graph[node]['prec'] <= processed
-                               if node not in processed)
+    @property
+    @abstractmethod
+    def has_worker(self):
+        pass
 
-        to_process.update(can_process_next)
-        order.append(current)
+    @abstractmethod
+    def do(self, task):
+        pass
 
-    return order
+    @abstractmethod
+    def get_finished(self):
+        pass
+
+    @property
+    @abstractmethod
+    def time(self):
+        pass
+
+
+class IdealDoer(StepDoer):
+
+    def __init__(self):
+        self._step = None
+
+    @property
+    def has_worker(self):
+        return self._step is None
+
+    def do(self, task):
+        assert self._step is None
+        self._step = task
+
+    def get_finished(self):
+        finished = (self._step, )
+        self._step = None
+        return finished
+
+    @property
+    def time(self):
+        return 0
+
+
+class CrappyDoer(StepDoer):
+
+    def __init__(self, num_workers, step_cost):
+        self._num_workers = num_workers
+        self._step_cost = step_cost
+        self._step_times = {}
+        self._time = 0
+
+    @property
+    def has_worker(self):
+        return len(self._step_times) < self._num_workers
+
+    def do(self, task):
+        self._step_times[task] = self._step_cost(task)
+
+    def get_finished(self):
+        min_time = min(self._step_times.values())
+        self._time += min_time
+        finished = [step for step, time in self._step_times.items() if time == min_time]
+        self._step_times = {step: time - min_time for step, time in self._step_times.items() if time != min_time}
+        return finished
+
+    @property
+    def time(self):
+        return self._time
+
+
+def do_all_steps(source, doer):
+    while not source.done:
+        while doer.has_worker:
+            task = source.get_next()
+            if task is None:
+                break
+            doer.do(task)
+        finished = doer.get_finished()
+        for task in finished:
+            source.finish(task)
 
 
 def main():
-    lines = utils.read_input('input7.txt')
-    graph = make_graph(lines)
-    order = order_nodes(graph)
-    print(''.join(order))
+    source = StepSource('input7.txt')
+    doer = IdealDoer()
+    do_all_steps(source, doer)
+    print(source.order)
+
+    source = StepSource('input7.txt')
+    doer = CrappyDoer(num_workers = 5, step_cost = lambda step: 60 + ord(step) - 64)
+    do_all_steps(source, doer)
+    print(doer.time)
 
 
 if __name__ == '__main__':
