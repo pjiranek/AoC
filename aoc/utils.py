f8870fe655c9947a0e00e1d60238d0c45532b703
Pavel Jiranek <pavel.jiranek@siemens.com>
2018-12-14 18:57:01 +0100

    Added a timing decorator.

--- aoc/utils.py
+++ aoc/utils.py
@@ -1,4 +1,19 @@
+import time
+
+
 def read_input(file_name):
     with open(file_name, 'r') as f:
-        stripped_lines = (line.strip() for line in f.readlines())
+        stripped_lines = (line.rstrip() for line in f.readlines())
         return [line for line in stripped_lines if line]
+
+
+def timeit(func):
+
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        func(*args, **kwargs)
+        end_time = time.time()
+
+        print('{0} took {1} seconds.'.format(func.__name__, end_time - start_time))
+
+    return wrapper
